c9c2344c59935c3c8f90a05183d95d7237a507c2
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-05-13 21:47:13 -0400

    Created Post component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  import React from 'react';
+  import Post from './components/Post'
+function App(){ 
+  return(
+    <div>
+      <h1>Fakebook!</h1>
+      
+        {/* CreatePostForm */}
+        
+        {/* Feed */}
+        
+        <Post />
+    
+    </div>
+  )
+}
+export default App
