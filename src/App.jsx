c245f42d938ec4258a3cd242021ec4972246bd0a
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-05-13 21:58:56 -0400

    Created Comment Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  import React from 'react';
-  import Post from './components/Post'
+import React from 'react';
+  import Post from './components/Post';
 function App(){ 
   return(
     <div>
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Comment from "./Comment";
 function Post(){
   return(
     <div>
@@ -6,6 +7,7 @@ function Post(){
       <p>This is a test post we'll edit later!</p>
       <p>Likes: 30</p>
       <p>Comments:</p>
+      <Comment />
     </div>
   )
 }
